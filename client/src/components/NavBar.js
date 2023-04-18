0a9bc2cdccd082ecdb1b050af411ff8e39e419c1
WillBridge0789 <will.burbridge@student.awesomeinc.org>
2023-04-18 15:51:21 +0000

    Changed color of navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,6 +1,6 @@
 function NavBar() {
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <nav className="navbar navbar bg-dark navbar-expand-lg" data-bs-theme="dark">
             <div className="container-fluid">
                 <a className="navbar-brand" href="#">Navbar</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
