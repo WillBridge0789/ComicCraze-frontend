519a83bdaaaf98bfe7c83db9cf74de99c496abcc
WillBridge0789 <will.burbridge@student.awesomeinc.org>
2023-04-27 13:50:34 +0000

    url constant changed

--- client/src/services/auth.constants.js
+++ client/src/services/auth.constants.js
@@ -1,4 +1,5 @@
-export const API_URL = "https://8000-willbridge0-comiccrazeb-ckt42wxy9y8.ws-us95.gitpod.io";
-export const LOGIN_ENDPOINT = "user/login/"
-export const REGISTER_ENDPOINT = "user/signup/"
-export const REFRESH_ENDPOINT = "token/refresh/"
+export const API_URL =
+  "https://8000-willbridge0-comiccrazeb-ckt42wxy9y8.ws-us96.gitpod.io";
+export const LOGIN_ENDPOINT = "user/login/";
+export const REGISTER_ENDPOINT = "user/signup/";
+export const REFRESH_ENDPOINT = "token/refresh/";
