24a2e00111263952e4cab82ae8763e385befa0cf
Ember Borja <ryan.t.borja@gmail.com>
2023-06-02 18:51:14 +0000

    Better error handling on user data response

--- client/src/components/Comics.js
+++ client/src/components/Comics.js
@@ -8,14 +8,13 @@ import "aos/dist/aos.css";
 import Footer from "./Footer";
 import { API_URL } from "../services/auth.constants";
 import { css } from "@emotion/react";
-import { ScaleLoader, GridLoader } from "react-spinners";
+import { GridLoader } from "react-spinners";
 
 function Comics() {
   const [comics, setComics] = useState([]);
   const [favorites, setFavorites] = useState([]);
   const [state, dispatch] = useGlobalState();
   const [cart, setCart] = useState(state.cart);
-  const [seriesFilter, setSeriesFilter] = useState("");
   const [isLoading, setIsLoading] = useState(true);
 
   const override = css`
@@ -58,8 +57,8 @@ function Comics() {
       axios
         .get(`${API_URL}/users/${state.currentUser.user_id}/`) // may need a new port link per project reload
         .then((response) => {
-          setFavorites(response.data.favorite_comics.map((c) => c.id));
-          setCart(response.data.cart_item.map((c) => c.id));
+          setFavorites(response?.data?.favorite_comics?.map((c) => c.id) || []);
+          setCart(response?.data?.cart_item?.map((c) => c.id) || []);
         });
     }
   }, []);
